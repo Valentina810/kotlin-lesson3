de3b146de910a05d54839a0b00202774ae88df83
valentinakolesnikova <valentinavasileva34@gmail.com>
2024-07-28 23:04:01 +0300

    feat: добавить часть упражнений по 3 уроку

--- src/main/kotlin/FunTest.kt
+++ src/main/kotlin/FunTest.kt
@@ -1,2 +1,18 @@
 package org.example
 
+fun main() {
+    println("Hello")
+
+    val initList = listOf(1, 5, 10, 22, 3)
+    println(initList.mySum())
+    println(initList.lastElem())
+}
+
+// есть массив целых чисел,
+// написать функцию-расширение, которая вернет его сумму
+fun List<Int>.mySum() = reduce { accumulator, number ->
+    accumulator + number
+}
+
+//написать свойство-расширение, которое отдает последний элемент list
+fun List<Int>.lastElem(): Int = get(size - 1)
